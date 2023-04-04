b69ed872345ec149c6b28c42b7644babd836de97
Vitalii <LVitek2506@yandex.ru>
2023-04-04 10:38:45 +0500

    initizialization

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int ReadInt(string message)
+{
+    Console.Write($"{message} >");
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+
+int SumNum(int )
